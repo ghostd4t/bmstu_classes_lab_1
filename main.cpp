d22b3bfdff0d195633207636402a9a780858a200
ghostd4t <abusygin076@gmail.com>
2022-11-28 10:54:55 +0300

    fixed output artifacts YYYEEESSS!!

--- main.cpp
+++ main.cpp
@@ -1,24 +1,28 @@
 /* main.cpp ---> transfer it to obj.C*/
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
 #include "students.h"
+
+
 int main()
 {
+
+    
     //Student
     Students student;
 
-    char name[100];
-    char last_name[100];
-    char group[100];
+    char name[15];
+    char last_name[15];
+    char group[15];
     
-    printf("Name: ");
+    printf("[+] Name: ");
     scanf("%s", &name);
-    printf("Last name: ");
+    printf("[+] Last name: ");
     scanf("%s",&last_name);
-    printf("Group: ");
+    printf("[+] Group: ");
     scanf("%s",&group);
     
-
     student.set_name(name);
     student.set_last_name(last_name);
     student.set_group(group);
@@ -26,7 +30,7 @@ int main()
     int scores[5];
     int sum = 0;
     for (int i = 0; i < 5; ++i) {
-        printf("Score %d: ", i+1);
+        printf("[+] Score %d: ", i+1);
         scanf("%d", &scores[i]);
         sum += scores[i];
     }
@@ -34,9 +38,8 @@ int main()
     float average_ball = sum / 5.0;
     student.set_average_ball(average_ball);
     //Student object created ^
-    printf("Average score for %s",student.get_name());
-    printf(" %s group ",student.get_last_name()); 
-    printf("%s ", student.get_group());
-    printf("is %.2f",student.get_average_ball());
+    printf("[+] Average score for %s %s group %s is [%.2f]",
+    student.get_name(), student.get_last_name(), 
+    student.get_group(), student.get_average_ball());
     return 0;
 }
--- students.h
+++ students.h
@@ -1,45 +1,41 @@
 #include <string.h>
+
 class Students {
     public:
         void set_name(char student_name[])
         {
-            for(int i = 0; i < strlen(student_name); i++){
-                printf("%c ", student_name[i]);
+            for(int i = 0; i <= strlen(student_name); i++){
                 name[i] = student_name[i];
+                
             }
-            printf("\n");
         }
-        char * get_name()
+        void * get_name()
         {
             return name;
         }
         void set_last_name(char student_last_name[])
         {
-            for(int i = 0; i < strlen(student_last_name); i++){
-                printf("%c ", student_last_name[i]);
+            for(int i = 0; i <= strlen(student_last_name); i++){
                 last_name[i] = student_last_name[i];
             }
-            printf("\n");
         }
-        char * get_last_name()
+        void * get_last_name()
         {
             return last_name;
         }
 
         void set_group(char group_number[]){
-            for(int i = 0; i < strlen(group_number); i++){
-                printf("%c ", group_number[i]);
+            for(int i = 0; i <= strlen(group_number); i++){
                 group[i] = group_number[i];
             }
-            printf("\n");
         }
 
-        char * get_group(){
+        void *get_group(){
             return group;
         }
         void set_scores(int student_scores[])
         {
-            for (int i = 0; i < 5; ++i) {
+            for (int i = 0; i <= 4; ++i) {
                 scores[i] = student_scores[i];
             }
         }
@@ -54,8 +50,8 @@ class Students {
 
     private:
         int scores[5];
-        float average_ball; 
-        char name[100];
-        char group[100];
-        char last_name[100];
+        float average_ball;
+        char name[15];
+        char group[15];
+        char last_name[15];
 };

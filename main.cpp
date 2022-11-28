35ec368d1d209b8b402b9c698f1d5ab8af2c2bad
ghostd4t <abusygin076@gmail.com>
2022-11-28 15:02:01 +0300

    sosal hui na pare 45 min

--- main.cpp
+++ main.cpp
@@ -6,15 +6,13 @@
 
 int main()
 {    
-    /*
     printf("[+] Amount of students: ");
-    int amount_students = 1;
-    scanf("%d", amount_students);
-    */
-    int const amount_students = 2;
-    
-    char *student_base_names[amount_students][3];
-    int *student_base_ses[amount_students][5];
+    int amount_students;
+    scanf("%d", &amount_students);
+    int save;
+    char *student_base_names[100];
+    int *student_base_ses[100][5];
+    char temp[100][100];
     /*
     int ses_base[1000][1000];
     */
@@ -48,23 +46,21 @@ int main()
         float average_ball = sum / 5.0;
         student.set_average_ball(average_ball);
         //Student object created ^
-      
-        student_base_names[cycle][0] = student.get_name();
-        student_base_names[cycle][1] = student.get_last_name();
-        student_base_names[cycle][2] = student.get_group();
-
-        for(int i = 0; i < 3; i++){
-            printf("%s ", student_base_names[cycle][i]);
+        printf("%s\n", student.get_name());
+        printf("%s\n", student.get_last_name());
+        printf("%s\n", student.get_group());
+        printf("%d\n", strlen(student.get_name()));
+        for(int i = 0; i < strlen(student.get_name()); i++){
+            temp[cycle][i] = student.get_name()[i];
         }
-        printf("\n");
-        for(int i = 0; i < 5; i++){
-            student_base_ses[cycle][i] = student.get_ses();
-        }
+        /* fix idea 
+        char sum[1000];
+        sum = student.get_name() + student.get_last_name() + student.get_group();
+        */
+        //student_base_names[cycle] = student.get_all_name();
     }
-    for(int k = 0; k < 2; k++){
-        for(int m = 0; m < 3; m++){
-            printf("%s", student_base_names[0][m]);
-        }
+    for(int i = 0; i < amount_students; i++){
+        printf("%s\n", temp[i]);
     }
     /*
     for(int i = 0; i < amount_students; i++){
